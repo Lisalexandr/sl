a03a7dec081669bb1b9cb44443ce115981e48b59
Alexandr Lisovitsky <alexandr.lisovitsky@gitlab.webamg.com>
2021-02-06 12:26:35 +0200

    fix:chrome ci

--- wdio.conf.js
+++ wdio.conf.js
@@ -115,9 +115,9 @@ exports.config = {
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
     services: [
-        ['chromedriver', {
-            args: ['--no-sandbox', '--disable-dev-shm-usage', '--headless']
-        }]
+        // ['chromedriver', {
+        //     args: ['--no-sandbox', '--disable-dev-shm-usage', '--headless']
+        // }]
     ],
     
     // Framework you want to run your specs with.

9cdc0382a43fa278b8be29eb0a8928ee02c6c644
Alexandr Lisovitsky <alexandr.lisovitsky@gitlab.webamg.com>
2021-02-06 12:37:13 +0200

    chore: chrome ci

--- wdio.conf.js
+++ wdio.conf.js
@@ -98,7 +98,8 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'http://127.0.0.1:8000/',
+    // baseUrl: 'http://127.0.0.1:8000/',
+    baseUrl: 'http://google.com/',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
